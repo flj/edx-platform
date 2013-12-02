6603b393a9a814c6ae7c1a04630d9c7794729ec0
David Baumgold <david@davidbaumgold.com>
2013-12-02 14:51:04 -0500

    Updating comments and docs for MITx -> edX transition
    Also whitespace changes

--- lms/envs/dev_ike.py
+++ lms/envs/dev_ike.py
@@ -4,7 +4,7 @@ sessions. Assumes structure:
 
 /envroot/
         /db   # This is where it'll write the database file
-        /mitx # The location of this repo
+        /edx-platform  # The location of this repo
         /log  # Where we're going to write log files
 """
 
@@ -51,4 +51,4 @@ MIDDLEWARE_CLASSES = tuple([mcl for mcl in MIDDLEWARE_CLASSES if not mcl.startsw
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    )
+)
