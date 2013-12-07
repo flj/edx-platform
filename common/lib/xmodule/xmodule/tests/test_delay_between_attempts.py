61df9a7ce94e3ed68293b9fb25aa9ecd8709f4eb
Jeff Ericson <jaericson@gmail.com>
2013-12-07 11:29:52 -0800

    Added comments to explain basis for test file

--- common/lib/xmodule/xmodule/tests/test_delay_between_attempts.py
+++ common/lib/xmodule/xmodule/tests/test_delay_between_attempts.py
@@ -1,5 +1,10 @@
 """
-Tests the logic of problems with a delay between attempt submissions
+Tests the logic of problems with a delay between attempt submissions.
+
+Note that this test file is based off of test_capa_module.py and as
+such, uses the same CapaFactory problem setup to test the functionality
+of the check_problem method of a capa module when the "delay between quiz
+submissions" setting is set to different values
 """
 
 import unittest
@@ -18,167 +23,10 @@ from . import get_test_system
 from pytz import UTC
 
 
-class CapaFactory(object):
-    """
-    A helper class to create problem modules with various parameters for testing.
-    """
-
-    sample_problem_xml = textwrap.dedent("""\
-        <?xml version="1.0"?>
-        <problem>
-            <text>
-                <p>What is pi, to two decimal places?</p>
-            </text>
-        <numericalresponse answer="3.14">
-        <textline math="1" size="30"/>
-        </numericalresponse>
-        </problem>
-    """)
-
-    num = 0
-
-    @classmethod
-    def next_num(cls):
-        """
-        Return the next cls number
-        """
-        cls.num += 1
-        return cls.num
-
-    @classmethod
-    def input_key(cls, input_num=2):
-        """
-        Return the input key to use when passing GET parameters
-        """
-        return ("input_" + cls.answer_key(input_num))
-
-    @classmethod
-    def answer_key(cls, input_num=2):
-        """
-        Return the key stored in the capa problem answer dict
-        """
-        return (
-            "%s_%d_1" % (
-                "-".join(['i4x', 'edX', 'capa_test', 'problem', 'SampleProblem%d' % cls.num]),
-                input_num,
-            )
-        )
-
-    @classmethod
-    def create(cls,
-               graceperiod=None,
-               due=None,
-               max_attempts=None,
-               showanswer=None,
-               rerandomize=None,
-               force_save_button=None,
-               attempts=None,
-               problem_state=None,
-               correct=False,
-               done=None,
-               text_customization=None,
-               last_submission_time=None,
-               submission_wait_seconds=None
-               ):
-        """
-        All parameters are optional, and are added to the created problem if specified.
-
-        Arguments:
-            graceperiod:
-            due:
-            max_attempts:
-            showanswer:
-            force_save_button:
-            rerandomize: all strings, as specified in the policy for the problem
-
-            problem_state: a dict to to be serialized into the instance_state of the
-                module.
-
-            attempts: also added to instance state.  Will be converted to an int.
-        """
-        location = Location(["i4x", "edX", "capa_test", "problem",
-                             "SampleProblem{0}".format(cls.next_num())])
-        field_data = {'data': cls.sample_problem_xml}
-
-        if graceperiod is not None:
-            field_data['graceperiod'] = graceperiod
-        if due is not None:
-            field_data['due'] = due
-        if max_attempts is not None:
-            field_data['max_attempts'] = max_attempts
-        if showanswer is not None:
-            field_data['showanswer'] = showanswer
-        if force_save_button is not None:
-            field_data['force_save_button'] = force_save_button
-        if rerandomize is not None:
-            field_data['rerandomize'] = rerandomize
-        if done is not None:
-            field_data['done'] = done
-        if text_customization is not None:
-            field_data['text_customization'] = text_customization
-        if last_submission_time is not None:
-            field_data['last_submission_time'] = last_submission_time
-        if submission_wait_seconds is not None:
-            field_data['submission_wait_seconds'] = submission_wait_seconds
-
-        descriptor = Mock(weight="1")
-        if problem_state is not None:
-            field_data.update(problem_state)
-        if attempts is not None:
-            # converting to int here because I keep putting "0" and "1" in the tests
-            # since everything else is a string.
-            field_data['attempts'] = int(attempts)
-
-        system = get_test_system()
-        system.render_template = Mock(return_value="<div>Test Template HTML</div>")
-        module = CapaModule(
-            descriptor,
-            system,
-            DictFieldData(field_data),
-            ScopeIds(None, None, location, location),
-        )
-
-        if correct:
-            # TODO: probably better to actually set the internal state properly, but...
-            module.get_score = lambda: {'score': 1, 'total': 1}
-        else:
-            module.get_score = lambda: {'score': 0, 'total': 1}
-
-        return module
-
-
 class XModuleQuizAttemptsDelayTest(unittest.TestCase):
     '''
-    Testing class
+    Actual class to test delay between quiz attempts
     '''
-
-    def setUp(self):
-        now = datetime.datetime.now(UTC)
-        day_delta = datetime.timedelta(days=1)
-        self.yesterday_str = str(now - day_delta)
-        self.today_str = str(now)
-        self.tomorrow_str = str(now + day_delta)
-
-        # in the capa grace period format, not in time delta format
-        self.two_day_delta_str = "2 days"
-
-    # def test_check_problem_resubmitted_with_randomize(self):
-    #     rerandomize_values = ['always', 'true']
-
-    #     for rerandomize in rerandomize_values:
-    #         # Randomize turned on
-    #         module = CapaFactory.create(rerandomize=rerandomize, attempts=0)
-
-    #         # Simulate that the problem is completed
-    #         module.done = True
-
-    #         # Expect that we cannot submit
-    #         with self.assertRaises(xmodule.exceptions.NotFoundError):
-    #             get_request_dict = {CapaFactory.input_key(): '3.14'}
-    #             module.check_problem(get_request_dict)
-
-    #         # Expect that number of attempts NOT incremented
-    #         self.assertEqual(module.attempts, 0)
 
     def test_first_submission(self):
         # Not attempted yet
@@ -431,3 +279,132 @@ class XModuleQuizAttemptsDelayTest(unittest.TestCase):
         # Also, the number of attempts should not be incremented
         self.assertRegexpMatches(result['success'], r"You must wait at least 1 minute between submissions. 1 minute remaining\..*")
         self.assertEqual(module.attempts, num_attempts)
+
+
+class CapaFactory(object):
+    """
+    A helper class to create problem modules with various parameters for testing.
+    """
+
+    sample_problem_xml = textwrap.dedent("""\
+        <?xml version="1.0"?>
+        <problem>
+            <text>
+                <p>What is pi, to two decimal places?</p>
+            </text>
+        <numericalresponse answer="3.14">
+        <textline math="1" size="30"/>
+        </numericalresponse>
+        </problem>
+    """)
+
+    num = 0
+
+    @classmethod
+    def next_num(cls):
+        """
+        Return the next cls number
+        """
+        cls.num += 1
+        return cls.num
+
+    @classmethod
+    def input_key(cls, input_num=2):
+        """
+        Return the input key to use when passing GET parameters
+        """
+        return ("input_" + cls.answer_key(input_num))
+
+    @classmethod
+    def answer_key(cls, input_num=2):
+        """
+        Return the key stored in the capa problem answer dict
+        """
+        return (
+            "%s_%d_1" % (
+                "-".join(['i4x', 'edX', 'capa_test', 'problem', 'SampleProblem%d' % cls.num]),
+                input_num,
+            )
+        )
+
+    @classmethod
+    def create(cls,
+               graceperiod=None,
+               due=None,
+               max_attempts=None,
+               showanswer=None,
+               rerandomize=None,
+               force_save_button=None,
+               attempts=None,
+               problem_state=None,
+               correct=False,
+               done=None,
+               text_customization=None,
+               last_submission_time=None,
+               submission_wait_seconds=None
+               ):
+        """
+        All parameters are optional, and are added to the created problem if specified.
+
+        Arguments:
+            graceperiod:
+            due:
+            max_attempts:
+            showanswer:
+            force_save_button:
+            rerandomize: all strings, as specified in the policy for the problem
+
+            problem_state: a dict to to be serialized into the instance_state of the
+                module.
+
+            attempts: also added to instance state.  Will be converted to an int.
+        """
+        location = Location(["i4x", "edX", "capa_test", "problem",
+                             "SampleProblem{0}".format(cls.next_num())])
+        field_data = {'data': cls.sample_problem_xml}
+
+        if graceperiod is not None:
+            field_data['graceperiod'] = graceperiod
+        if due is not None:
+            field_data['due'] = due
+        if max_attempts is not None:
+            field_data['max_attempts'] = max_attempts
+        if showanswer is not None:
+            field_data['showanswer'] = showanswer
+        if force_save_button is not None:
+            field_data['force_save_button'] = force_save_button
+        if rerandomize is not None:
+            field_data['rerandomize'] = rerandomize
+        if done is not None:
+            field_data['done'] = done
+        if text_customization is not None:
+            field_data['text_customization'] = text_customization
+        if last_submission_time is not None:
+            field_data['last_submission_time'] = last_submission_time
+        if submission_wait_seconds is not None:
+            field_data['submission_wait_seconds'] = submission_wait_seconds
+
+        descriptor = Mock(weight="1")
+        if problem_state is not None:
+            field_data.update(problem_state)
+        if attempts is not None:
+            # converting to int here because I keep putting "0" and "1" in the tests
+            # since everything else is a string.
+            field_data['attempts'] = int(attempts)
+
+        system = get_test_system()
+        system.render_template = Mock(return_value="<div>Test Template HTML</div>")
+        module = CapaModule(
+            descriptor,
+            system,
+            DictFieldData(field_data),
+            ScopeIds(None, None, location, location),
+        )
+
+        if correct:
+            # TODO: probably better to actually set the internal state properly, but...
+            module.get_score = lambda: {'score': 1, 'total': 1}
+        else:
+            module.get_score = lambda: {'score': 0, 'total': 1}
+
+        return module
